ee55ef04f9e795ddf3370cfe8f11252d19d197d2
Blade Smart <smartblader@gmail.com>
2020-06-09 18:46:20 +0500

    Add wrapper for 'nvdaController_cancelSpeech'

--- nvda/nvda.cpp
+++ nvda/nvda.cpp
@@ -16,11 +16,15 @@ std::wstring toWideChar(const char* c)
 
 static char SpeakText[] = "SpeakText";
 static char SpeakText_doc[] = "SpeakText(text)";
+static char CancelSpeech[] = "CancelSpeech";
+static char CancelSpeech_doc[] = "CancelSpeech()";
 
 static PyObject* nvda_SpeakText(PyObject* self, PyObject* args);
+static PyObject* nvda_CancelSpeech(PyObject* self, PyObject* args);
 
 static PyMethodDef methods[] = {
     { SpeakText, nvda_SpeakText, METH_VARARGS, SpeakText_doc },
+    { CancelSpeech, nvda_CancelSpeech, METH_VARARGS, CancelSpeech_doc },
     { NULL, NULL, 0, NULL },
 };
 
@@ -33,6 +37,18 @@ PyObject* nvda_SpeakText(PyObject* self, PyObject* args) {
         return NULL;
 
     int error_code = nvdaController_speakText(toWideChar(text).c_str());
+
+    return Py_BuildValue(i, error_code);
+}
+
+PyObject* nvda_CancelSpeech(PyObject* self, PyObject* args) {
+    char empty_string[] = "";
+    char i[] = "i";
+
+    if (!PyArg_ParseTuple(args, empty_string))
+        return NULL;
+
+    int error_code = nvdaController_cancelSpeech();
 
     return Py_BuildValue(i, error_code);
 }
